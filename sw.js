683ce0dcde017bd8f67872f212c9867192061b0b
SUMAN BANERJEE <sumaneducator10@gmail.com>
2025-11-01 17:41:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+// Service Worker for MAMATA FLY
+const CACHE_NAME = 'mamata-fly-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './script.js',
+  './style.css',
+  './mamta_-removebg-preview.png',
+  './modi.png',
+  './lastpic.png',
+  './mamata_voice.mp3',
+  './last.mp3'
+];
+
+// Install event - cache all resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
